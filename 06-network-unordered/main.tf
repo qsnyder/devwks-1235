346df04ff53a8bd84cb82d5c46fbaf6a658bb5ea
quinn snyder <snyderq@gmail.com>
2022-06-09 20:36:35 -0700

    Final cleanup of 06

--- 06-network-unordered/main.tf
+++ 06-network-unordered/main.tf
@@ -18,26 +18,26 @@ provider "aci" {
 resource "aci_bridge_domain" "terraform_bd" {
     tenant_dn          = aci_tenant.terraform_tenant.id
     relation_fv_rs_ctx = aci_vrf.terraform_vrf.id
-    description        = "PCUG-6 BD"
+    description        = "DEVWKS-1235 Demo BD"
     name               = var.bd
 }
 
 # Tenant BD's subnet
 resource "aci_subnet" "terraform_bd_subnet" {
     parent_dn   = aci_bridge_domain.terraform_bd.id
-    description = "PCUG-6 Subnet"
+    description = "DEVWKS-1235 Demo Subnet"
     ip          = var.subnet
 }
 
 # Tenant VRF (CTX)
 resource "aci_vrf" "terraform_vrf" {
     tenant_dn   = aci_tenant.terraform_tenant.id
-    description = "PCUG-6 VRF"
+    description = "DEVWKS-1235 Demo VRF"
     name        = var.vrf
 }
 
 # Define desired ACI tenant
 resource "aci_tenant" "terraform_tenant" {
     name        = var.tenant
-    description = "Tenant-6 for TF demo with PCUG"
+    description = "DEVWKS-1235 Demo Tenant"
 }
